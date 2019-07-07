b2bfeec224f9227e686d30cc33a130e72ae74911
eric <fantasy_2003_@hotmail.com>
2019-07-07 21:25:11 +0800

    add appveyor

---
+++ ci/appveyor/buildTestCI.ps1
@@ -0,0 +1,15 @@
+function BuildTestCI() {
+    # Go to build directory and build
+    # Build TestCI with cmake
+    md build
+    cd build
+    cmake -G "Visual Studio 14 2015 Win64"
+    cmake --build .
+    cd ..
+}
+
+function main() {
+    BuildTestCI
+}
+
+main
