43c1cc7d37ec640a0566ad6383dadcad530698cb
eric <fantasy_2003_@hotmail.com>
2019-07-11 16:35:59 +0800

    Update installMPI.ps1

--- ci/appveyor/installMPI.ps1
+++ ci/appveyor/installMPI.ps1
@@ -22,9 +22,11 @@ function InstallCmake() {
     $exePath = "$($env:USERPROFILE)/cmake-3.14.5-win64-x64.msi"
     Write-Host "Downloading..."
     #(New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.4/cmake-3.4.0-rc2-win32-x86.exe', $exePath)
-    (New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-win64-x64.msi', $exePath)
+    #(New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-win64-x64.msi', $exePath)
+    appveyor DownloadFile https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-win64-x64.msi
     Write-Host "Installing..."
-    cmd /c start /wait $exePath /S
+    #cmd /c start /wait $exePath /S
+    Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i cmake-3.14.5-win64-x64.msi" -Wait
     cmake --version
     Write-Host "CMake 3.14.5 installed" -ForegroundColor Green
 }
