85699adc5e25573749dc97df80549baa0b284828
eric <fantasy_2003_@hotmail.com>
2019-07-11 15:37:46 +0800

    Update buildTestCI.ps1

--- ci/appveyor/buildTestCI.ps1
+++ ci/appveyor/buildTestCI.ps1
@@ -3,7 +3,7 @@ function BuildTestCI() {
     # Build TestCI with cmake
     md build
     cd build
-    cmake -G "Visual Studio 14 2015 Win64" ../
+    cmake -G "Visual Studio 14 2015 Win64" -DMPI_DIR="C:/Program Files (x86)/Microsoft SDKs/MPI" ../
     cmake --build . --target INSTALL --config release
 #    dir
 #    msbuild Test.sln
