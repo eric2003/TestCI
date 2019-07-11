632316b0a5638c6029d101a9605f2431ac3c333e
eric <fantasy_2003_@hotmail.com>
2019-07-11 18:53:10 +0800

    Update installMPI.ps1

--- ci/appveyor/installMPI.ps1
+++ ci/appveyor/installMPI.ps1
@@ -17,11 +17,22 @@ function InstallMPI() {
     dir
 }
 
+#function InstallCmake() {
+#    Write-Host "Installing CMake 3.4.0 ..." -ForegroundColor Cyan
+#    $exePath = "$($env:USERPROFILE)/cmake-3.4.0-rc2-win32-x86.exe"
+#    Write-Host "Downloading..."
+#    (New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.4/cmake-3.4.0-rc2-win32-x86.exe', $exePath)
+#    Write-Host "Installing..."
+#    cmd /c start /wait $exePath /S
+#    cmake --version
+#    Write-Host "CMake 3.4.0 installed" -ForegroundColor Green
+#}
+
 function InstallCmake() {
     Write-Host "Installing CMake 3.4.0 ..." -ForegroundColor Cyan
-    $exePath = "$($env:USERPROFILE)/cmake-3.4.0-rc2-win32-x86.exe"
+    $exePath = "$($env:USERPROFILE)/cmake-3.15.0-rc4-win64-x64.msi"
     Write-Host "Downloading..."
-    (New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.4/cmake-3.4.0-rc2-win32-x86.exe', $exePath)
+    #(New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.15.0-rc4/cmake-3.15.0-rc4-win64-x64.msi', $exePath)
     Write-Host "Installing..."
     cmd /c start /wait $exePath /S
     cmake --version
