ba0f962704b0de25f36b2e66e9a72e47879775f9
eric <fantasy_2003_@hotmail.com>
2019-07-21 05:23:29 +0800

    MPI_INT

--- codes/main.cpp
+++ codes/main.cpp
@@ -121,7 +121,8 @@ int main( int argc, char ** argv )
     cout << "To you : Process " << myid << "of" << numprocess << " on " << processor_name << "\n";
     int n = 1;
     int m = 0;
-    MPI_Allreduce( &n, &m, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD );
+    //MPI_Allreduce( &n, &m, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD );
+    MPI_Allreduce( &n, &m, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
     cout << "myid = " << myid << " n = " << n << " m = " << m << "\n";
 
     MPI_Finalize();
